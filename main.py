b39cfe0e36850500e56b2a2647ba7cab56243a4b
kimteespk <supakorn.saeiab@gmail.com>
2023-12-17 16:49:52 +0700

    model template

--- main.py
+++ main.py
@@ -6,6 +6,14 @@ from processing import preprocessing_pipeline, MultiKeyShift# ,shift_back_data
 from processing import learn_pred, create_metric, create_pipeline
 
 
+## DEV MODULE
+from pandas import to_datetime
+
+
+# DUMMY DATAKEY
+data_key = ['openPrice', 'highPrice', 'lowPrice', 'lastPrice', 'priceChangePercent']
+y_key = 'lastPrice'
+window_size = 300
 
 ## READ DATA FROM CSV
 # header 
@@ -29,20 +37,19 @@ def get_data_dev(path= './dataset/realtime/raw_ethusdt_1m.csv', nrow= 100):
     return lst_dct
 
 
-lst_raw_data = get_data_dev()
+lst_raw_data = get_data_dev(nrow= 3000)
 
 
 ######  EXCLUDE FROM CLASS METHOD
 # get data key key
-rec = lst_raw_data[0].copy()
-data_key = list(rec.keys())
+# rec = lst_raw_data[0].copy()
+# data_key = list(rec.keys())
 
-# DUMMY DATAKEY
-data_key = ['openPrice', 'highPrice', 'lowPrice', 'lastPrice', 'quoteVolume', 'priceChangePercent']
-y_key = 'lastPrice'
+
 
 ## INIT MULTISHEY SHIFT 
-multi_key_shift = MultiKeyShift(keys= data_key, window_size= 2, key_exclude= y_key)
+
+multi_key_shift = MultiKeyShift(keys= data_key, window_size= window_size, key_exclude= y_key)
 ## INIT PIPELINE
 model_pl = create_pipeline()
 ## INIT METRIC
@@ -53,56 +60,91 @@ lst_result = []
 dct_result = {}
 
 for record in lst_raw_data:
-    print(record)
-    # print('brefore preprocess :', record)
+    # print(record)
+    # print('\nopenPrice brefore preprocess :', record['openPrice'])
     prep_rec = preprocessing_pipeline(record)
-    data_ = {}
-    data_['openPrice'] = prep_rec['openPrice']
-    data_['highPrice'] = prep_rec['highPrice']
-    data_['lowPrice'] = prep_rec['lowPrice']
-    data_['lastPrice'] = prep_rec['lastPrice']
-    data_['quoteVolume'] = prep_rec['quoteVolume']
-    data_['priceChangePercent'] = prep_rec['priceChangePercent']
+    data_ = {key: prep_rec[key] for key in data_key}
+    data_['lastPrice'] = record['lastPrice']
+    
     prep_rec = data_
     
     prep_rec = multi_key_shift.update(prep_rec)
     prep_rec = multi_key_shift.get()
-    print('\n after preprocess :', prep_rec)
-    y = prep_rec.pop(y_key)
-    
-    # LEARN AND PREDICT
-    print('### ', model_pl)
-    x, y, y_pred, model_pl, model_metric = learn_pred(x= prep_rec, y= y, pl= model_pl, metric= model_metric)
 
-    # print(prep_rec['closeTime'])
+    y = prep_rec.pop(y_key)
+
+    
+    ## LEARN AND PREDICT
+    try:
+        _x, y, y_pred, model_pl, model_metric = learn_pred(x= prep_rec, y= y, pl= model_pl, metric= model_metric)
+    except Exception as e:
+        print(e)
+        continue
+    
+
     dct_result = {
+        'closeTime': record['closeTime'],
         'y_actual': y,
         'y_predict': y_pred,
         'MAE': model_metric
     }
+    if  y_pred != None:
+        # continue
+        lst_result.append(dct_result)
+        # print(dct_result['MAE'])
+        print(dct_result)
     
-    lst_result.append(dct_result)
+########################## PLOT ######################################
 
-print(lst_result)
+import matplotlib.pyplot as plt
+from pandas import to_datetime
+import matplotlib.dates as mdates
 
-############################# REF ##############################
-# #BEFORE EXCLUDE Y
-# get x key
-# rec = lst_raw_data[0].copy()
-# rec.pop('lastPrice')
-# data_key = list(rec.keys())
+def plot_result(result):
+    global window_size
+    # Assuming lst_result is a list of dictionaries
+    # Extract 'closeTime', 'y_actual', 'y_predict', and 'MAE' from each dictionary
+    close_times = [to_datetime(result['closeTime'], unit='ms') for result in lst_result]
+    y_actual_values = [result['y_actual'] for result in lst_result]
+    # y_predict_values = pd.Series([x['y_predict'] for x in lst_result]).shift(window_size).tolist()
+    y_predict_values = [result['y_predict'] for result in lst_result]
+    mae_values = [result['MAE'].get() for result in lst_result]
 
-# multi_key_shift = MultiKeyShift(keys= data_key, window_size= 2)
+    # Plot the line graph with each second as a data point
+    fig, ax1 = plt.subplots(figsize=(10, 6))
 
-# for record in lst_raw_data:
-#     # print('brefore preprocess :', record)
-#     prep_rec = preprocessing_pipeline(record)
-#     lst_price = prep_rec.pop('lastPrice')
-#     print('last price :', lst_price)
+    # Plot actual and predicted values
+    ax1.plot(close_times, y_actual_values, label='Actual', marker='o', linestyle='-', markersize=5)
+    ax1.plot(close_times, y_predict_values, label='Predicted', marker='x', linestyle='-', markersize=5)
+
+    # Set labels and title for the first y-axis
+    ax1.set_xlabel('Close Time')
+    ax1.set_ylabel('Values', color='black')
+    ax1.set_title('Actual vs Predicted Values over Time')
+
+    # Show legend for the first y-axis
+    ax1.legend(loc='upper left')
+
+    # Create a second y-axis for MAE values
+    ax2 = ax1.twinx()
+    ax2.plot(close_times, mae_values, label='MAE', marker='x', linestyle='-', color='red', markersize=5)
+
+    # Set labels and title for the second y-axis
+    ax2.set_ylabel('MAE', color='red')
+
+    # Show legend for the second y-axis
+    ax2.legend(loc='upper right')
+
+    # Rotate x-axis labels for better readability
+    plt.xticks(rotation=45)
+
+    # Set the x-axis format to show date and time
+    ax1.xaxis.set_major_formatter(mdates.DateFormatter('%Y-%m-%d %H:%M:%S'))
+
+    # Show the plot
+    plt.show()
+
     
-#     prep_rec = multi_key_shift.update(prep_rec)
-#     prep_rec = multi_key_shift.get()
-#     prep_rec['lastPrice'] = lst_price
-#     print('\n after preprocess :', prep_rec)
-    
+plot_result(lst_result)
 
+############################################################################################################
